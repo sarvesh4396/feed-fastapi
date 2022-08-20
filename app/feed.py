a646db966183f77bfd74d856ebd5a450cb10f0ad
sarvesh4396 <heysarvesh@pm.me>
2022-08-20 16:03:23 +0530

    add deta url

--- app/feed.py
+++ app/feed.py
@@ -9,7 +9,7 @@ html = """
         <ul id='messages'>
         </ul>
         <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
+            var ws = new WebSocket("ws://1h86jt.deta.dev/ws");
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
